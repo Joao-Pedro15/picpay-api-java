b7cc34362b2aa45bc66f419122b66a3dc564d034
Joao Pedro <joao.monteiro@veredaeducacao.com>
2024-08-02 09:31:10 -0300

    feat:create new method get by email and encrypt password with bcrypt

--- src/main/java/com/picpaysimplificado/repositories/UserRepository.java
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.picpaysimplificado.repositories;
 
 import com.picpaysimplificado.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
     Optional<User> findUserById(Long id);
+    UserDetails findByEmail(String email);
 }
